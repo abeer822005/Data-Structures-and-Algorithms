1d3582e9a468b665e6f1196c91a1692fd252d360
abeer822005 <22220006@scme.edu.ps>
2024-11-03 09:51:52 +0200

    Create list2.py

---
+++ list/list2.py
@@ -0,0 +1,12 @@
+list1=[2,3]
+list2=[1,9]
+list3=list1+list2
+print("the list3 is ",list3)
+list3=3*list1
+print (list3)
+list4=list3[2:4]
+print (list4)
+list1[-1]
+print(list1)
+list5=[2,3,5,2,33,21]
+print (list5[-1])
